afcd372fb989fb32ac1d810dd4c3fe5d7dc70f91
victor.costa <vicpc42@gmail.com>
2024-04-17 14:40:42 -0300

    Script was untested. Fixed it

--- filter_dataset.py
+++ filter_dataset.py
@@ -3,33 +3,34 @@ import shutil
 
 def main():
     dataset_folder = '/home/victor.costa/data/alcaim'
-    out_folder = '../cetuc-filtered'
-    cluster_select_file = "alcaim_clusters/cluster-format.txt"
-    cluster_folder = "alcaim_clusters/clusters"
+    output_folder = '../cetuc-filtered'
+    cluster_select_file = "clusters-format.txt"
+    cluster_folder = "clusters"
     
     clusters = {}
     with open(cluster_select_file, "r") as select_f:
-        for line in select_f.readline():
+        for line in select_f.readlines():
             spk, cluster_number, cluster_id, _ = line.split(':')
             clusters[spk] = set()
-            with open(f"clusters-{spk}-{cluster_number}.txt", "r") as cluster_f:
+            with open(os.path.join(cluster_folder, f"clusters-{spk}-{cluster_number}.txt"), "r") as cluster_f:
                 for l in cluster_f.readlines():
-                    fn, cl = l.split(':')
+                    fn, cl = l.strip().split(':')
                     if cl == cluster_id:
                         clusters[spk].add(os.path.basename(fn))
                         
-    os.makedirs(dataset_out, exist_ok=True)
+    os.makedirs(output_folder, exist_ok=True)
     
     for spk in os.listdir(dataset_folder):
         in_folder = os.path.join(dataset_folder, spk)
-        out_folder = os.path.join(out_folder, spk)
+        out_folder = os.path.join(output_folder, spk)
         if not os.path.isdir(in_folder):
             continue
-        os.path.makedirs(out_folder, exist_ok=True)
+        os.makedirs(out_folder, exist_ok=True)
         for fn in os.listdir(in_folder):
+            #print(fn)
             if os.path.splitext(fn)[1] != '.wav' or fn not in clusters[spk]:
                 continue
-            shitil.copy2(os.path.join(in_folder, fn), os.path.join(out_folder, fn))
+            shutil.copy2(os.path.join(in_folder, fn), os.path.join(out_folder, fn))
                 
     
     
